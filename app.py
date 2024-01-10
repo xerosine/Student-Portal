4c4d56fb99e17c3877a262ec4b9dc1be90755fab
Ayanga Oluwamurewa <phoenixflower2005@gmail.com>
2024-01-10 22:40:27 +0100

    First commit (Added landing and register pages)

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for, request, flash, current_app
+import json, pymysql, os
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+@app.route('/')
+def landing():
+    return render_template('LandingPage.html')
+
+@app.route('/register')
+def register():
+    return render_template('FormPage.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
