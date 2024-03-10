ca27d5e62011c2cc30f847609b6dc4473b60ad49
Ayanga Oluwamurewa <ayangaoluwamurewa@gmail.com>
2024-03-10 23:46:19 +0100

    rearranged structure

--- app.py
+++ app.py
@@ -4,6 +4,20 @@ from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
+# timeout = 10
+# connection = pymysql.connect(
+#   charset="utf8mb4",
+#   connect_timeout=timeout,
+#   cursorclass=pymysql.cursors.DictCursor,
+#   db="defaultdb",
+#   host="mysql-1642f47c-ayangaoluwamurewa-8ec6.a.aivencloud.com",
+#   password="AVNS_b4us0vHkXgy29lgGlq2",
+#   read_timeout=timeout,
+#   port=10343,
+#   user="avnadmin",
+#   write_timeout=timeout,
+# )
+
 @app.route('/')
 def landing():
     return render_template('LandingPage.html')
@@ -12,9 +26,13 @@ def landing():
 def register():
     return render_template('FormPage.html')
 
-@app.route('/allDetails')
-def allDetails():
+@app.route('/details')
+def details():
     return render_template('AllDetails.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profilePage.html')
 
 
 if __name__ == "__main__":
